2c6d79c5e01d8aae37a635eebf15b486d03b2624
Frances <frances_julianne_delacruz@dlsu.edu.ph>
2026-03-16 01:10:43 +0800

    feat: created sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,19 @@
+export default function sitemap() {
+    const baseUrl = "https://portfolio-frances-delacruz.vercel.app";
+
+    return [
+        {
+            url: `${baseUrl}/`,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 1.0,
+        },
+        {
+            url: `${baseUrl}/contact`,
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 0.5,
+        },
+    ];
+}
+
